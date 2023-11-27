a3ee3838261d4f818d60a042364dd8ce0acc78a1
lucazmartins <140116186+lucazmartins@users.noreply.github.com>
2023-11-27 12:46:06 -0300

    Definindo os parametros dos if's

--- calculadora-idade.js
+++ calculadora-idade.js
@@ -1,5 +1,47 @@
-function calculaIdade (dia, mes, ano){
-    return 22
+function calculaIdade(dia,mes,ano){
+    const anoCorrente = 2023;
+    const mesCorrente = 11;
+    const diaCorrente = 27;
 
-}
+    const subAno = anoCorrente - ano
+    const subMes = mesCorrente - mes
+    const subDia = diaCorrente - dia
+
+
+     
+     if(subAno <= 0)
+         return 0;
+
+    if (subMes > 0) 
+    return subAno     
+     
+     if(subMes < 0) {
+         return subAno - 1;
+     }
+     else {
+         if(subMes ==0) {
+             if(subDia  >0 ){
+                 return subDia
+             }
+             else {
+                if(subDia > 0)
+                return subAno
+                 
+             }
+         }
+         else {
+             
+         }
+     }
+ }
+
+
+
+
+
+
 module.exports = calculaIdade;
+
+
+// else = se nao 
+// if = se 
--- calculadora-idade.test.js
+++ calculadora-idade.test.js
@@ -1,15 +1,15 @@
 const calculaIdade = require('./calculadora-idade');
 
 test('verificar idade de Lucas', () => {
-  expect(calculaIdade(10, 9, 2001)).toBe(22);
+  expect(calculaIdade(22, 7, 1987)).toBe();
 });
 test('verificar idade de Juliana', () => {
-    expect(calculaIdade(8, 2, 2001)).toBe(22);
+    expect(calculaIdade(27, 11, 1980)).toBe();
   });
   test('verificar idade de Amelia', () => {
-    expect(calculaIdade(14, 3, 2020)).toBe(3);
+    expect(calculaIdade(28, 12, 1980)).toBe();
   });
   test('verificar idade de Lilly', () => {
-    expect(calculaIdade(14, 4, 2020)).toBe(3);
+    expect(calculaIdade(30, 11, 1980)).toBe();
   });
       
